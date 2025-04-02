e33f715073af1bf4bfe4db2263b3649efa27cc27
Chen Yuxuan <642962313@qq.com>
2025-04-02 01:39:59 +0100

    Completed Quiz1.py

---
+++ Quiz1.py
@@ -0,0 +1,27 @@
+# NUIST Ouiz Game in Python
+def quiz():
+    print("Welcome to the Animal Quiz!")
+    print("Answer the following questions:")
+# Questions and Answers
+questions =[
+"1. What is the largest animal on Earth?: a. Blue Whale, b. Mouse, c. cat",
+"2. Which bird can fly backwards?: a. Cuckoo, b. Eagle, c. Hummingbird ",
+"3. What is the only mammal capable of flight?: a. Bat, b. squirrel, c. Dolphin " ]
+answers = [
+"blue whale",
+"hummingbird",
+"bat"]
+score =0
+# Ask questions
+for i in range(len(questions)):
+    user_answer = input(questions[i]).strip().lower()
+    if user_answer == answers[i]:
+        print("correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+# Provide final score
+print("\nOuiz completed!")
+print(f"You got {score}/{len(questions)} questions correct.")
+# Run the quiz function
+quiz()
